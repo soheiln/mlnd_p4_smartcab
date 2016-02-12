a6ada8931ac97d630e3e9fb1c28d48c8ea0c924a
Soheil Norouzi <soheil.nrz@gmail.com>
2016-02-12 14:06:11 -0800

    checkpoint 4

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -14,10 +14,14 @@ class LearningAgent(Agent):
         # TODO: Initialize any additional variables here
         self.Q = {}
         self.QL_iterations = 100
+        self.state = None
+        self.total_rewards = 0
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
+        self.state = None
+        self.total_rewards = 0
 
     def update(self, t):
         # Gather inputs
@@ -38,46 +42,72 @@ class LearningAgent(Agent):
         delta_y = destination[1] - location[1]
         delta = (delta_x, delta_y)
 
+        can_execute = False;
+        if (self.next_waypoint == 'forward' and can_go_forward) or \
+            (self.next_waypoint == 'left' and can_go_left) or \
+            (self.next_waypoint == 'right' and can_go_right):
+            can_execute = True
+
         # TODO: Update state
-        s = (self.next_waypoint, inputs, deadline)
+        # TODO: can't have dict in state since it's unhashable
+        # s = (self.next_waypoint, inputs, deadline)
+        s = (self.next_waypoint, can_execute, deadline)
+        
+        # for printing the state on screen
+        self.state = "s:{}  total rewards:{}  deadline:{} ".format(s, self.total_rewards, deadline) 
+
 
         # TODO: Select action according to your policy
+        Q = self.Q
         optimal_action = random.choice(Environment.valid_actions)
-        q_max = Q[(s, optimal_action)]
+        q_max = 0
+        if Q.has_key( (s, optimal_action) ):
+            q_max = Q[(s, optimal_action)]
         for a in Environment.valid_actions:
-            if Q[(s,a)] > q_max:
+            if Q.has_key((s,a)) and Q[(s,a)] > q_max:
                 optimal_action = a
-                q_max = Q(s,a)
+                q_max = Q[(s,a)]
 
         # Execute action and get reward
         action = optimal_action
         reward = self.env.act(self, action)
+        self.total_rewards += reward
 
         # TODO: Learn policy based on state, action, reward
 
         # Q-Learning Parameters
-        self.learning_rate = 0.2
+        self.learning_rate = 0.35
         self.discount_factor = 0.8
 
         # calculating optimal future value for s(t+1) used in Q learning , in this case an expected value
         # since future state is not deterministic and could have 2^8 = 256 outcomes
+
         total_q = {}
         for a in Environment.valid_actions:
             total_q[a] = 0
-            for s_ in Q.keys():
-                if s_[3] != deadline-1:
+            for (s_,a_) in Q.keys():
+                if s_[2] != deadline-1:
                     pass
                 else:
-                    total_q += Q[s_]
-            total_q[a] /= 256 # getting the expected value. there are 2^8 options for next_waypoint and inputs in the new state
+                    print "s_: {}".format(s_)
+                    total_q[a] += Q[(s_,a_)]
+            total_q[a] /= 32 # getting the expected value
 
         optimal_future_value = max(total_q.values())
+
+        print "\n\nnext_waypoint: {}".format(self.next_waypoint)
+        # print "Q: {}".format(Q)
+        print "self.QL_iterations: {}".format(self.QL_iterations)
+        print "optimal_action: {}".format(optimal_action)
+        print "optimal_future_value: {}".format(optimal_future_value)
 
         q_old = 0
         if( Q.has_key((s,a)) ):
             q_old = Q[(s,a)]
 
-        Q[(s, a)] = q_old + self.learning_rate * (reward + self.discount_factor * optimal_future_value - q_old)
+        if self.QL_iterations > 0:
+            self.QL_iterations -= 1
+            Q[(s, a)] = q_old + self.learning_rate * (reward + self.discount_factor * optimal_future_value - q_old)
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
